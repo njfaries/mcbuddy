613e2a1c8c73e309920b4ab7ffe67611f25935a8
Carl Patenaude Poulin <carl.patenaudepoulin@mail.mcgill.ca>
2015-01-27 13:00:26 -0500

    registration info extraction

--- prototype/app.js
+++ prototype/app.js
@@ -9,6 +9,19 @@ var CONFIG = {
 }
 
 QB.init(CONFIG.appID, CONFIG.authKey, CONFIG.authSecret, CONFIG.debug);
+
+$("#registrationForm").submit(function(evt) {
+	evt.preventDefault();
+	var info = 
+		$(this)
+		.children("input[type!='submit']")
+		.map(function(i, v) {
+			return { name : $(v).attr("name"), value : $(v).val() };
+		})
+		.get();
+	console.log(info);
+});
+
 QB.createSession(function(err, result) {
     var params = {login: 'garry', password: 'garry5santos'};
 
